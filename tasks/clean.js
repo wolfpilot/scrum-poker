061c8e07bb8729ef1240b20752f5290ac0a0a56a
Razvan Negrea <rznart@gmail.com>
2017-08-10 08:44:08 +0200

    chore(deploy): Add dist as a submodule

--- tasks/clean.js
+++ tasks/clean.js
@@ -5,6 +5,13 @@ import gulp from 'gulp';
 /**
  * Task: Clean dist/ folder
  */
+
+console.log(`!${config.dist.base}/.git`);
 gulp.task('clean', () => {
-    del.sync(config.dist.base);
+    del.sync(
+        [
+            `config.dist.base/**/*`,
+            `!${config.dist.base}/.git`
+        ]
+    );
 });
